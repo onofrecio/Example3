e31a5dbd6e53c18e13d8c878b8b1dad5fe36448e
onofrecio <alexlorman@hotmail.com>
2024-09-02 15:04:27 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,269 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-135_maxrossi8228_01_kitchen_2",
+      "name": "135_maxrossi8228_01_kitchen_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.675336362049407,
+          "pitch": 0.20713869007935237,
+          "rotation": 6.283185307179586,
+          "target": "2-135_maxrossi8228_01_pasillo_2"
+        },
+        {
+          "yaw": -2.1694173509998063,
+          "pitch": 0.26064808005666507,
+          "rotation": 0,
+          "target": "1-135_maxrossi8228_01_living_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-135_maxrossi8228_01_living_2",
+      "name": "135_maxrossi8228_01_Living_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1107460517507128,
+          "pitch": 0.4218687228921212,
+          "rotation": 0,
+          "target": "2-135_maxrossi8228_01_pasillo_2"
+        },
+        {
+          "yaw": 2.2560275686235283,
+          "pitch": 0.35816888088635324,
+          "rotation": 0,
+          "target": "0-135_maxrossi8228_01_kitchen_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-135_maxrossi8228_01_pasillo_2",
+      "name": "135_maxrossi8228_01_Pasillo_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5031495051315336,
+          "pitch": 0.6039357998901558,
+          "rotation": 0,
+          "target": "5-135_maxrossi8228_01_d2_2"
+        },
+        {
+          "yaw": 0.04294184070932161,
+          "pitch": 0.4128643027643406,
+          "rotation": 0,
+          "target": "4-135_maxrossi8228_01_d1_2"
+        },
+        {
+          "yaw": 0.8891214252239141,
+          "pitch": 0.6175764379198849,
+          "rotation": 0,
+          "target": "3-135_maxrossi8228_01_bath_2"
+        },
+        {
+          "yaw": 3.0603898326157175,
+          "pitch": 0.40090211029238176,
+          "rotation": 0,
+          "target": "1-135_maxrossi8228_01_living_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-135_maxrossi8228_01_bath_2",
+      "name": "135_maxrossi8228_01_Bath_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4862263925816777,
+          "pitch": 0.9683475297826796,
+          "rotation": 0,
+          "target": "2-135_maxrossi8228_01_pasillo_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-135_maxrossi8228_01_d1_2",
+      "name": "135_maxrossi8228_01_D1_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6729012988836676,
+          "pitch": 0.30536917433354205,
+          "rotation": 0,
+          "target": "2-135_maxrossi8228_01_pasillo_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-135_maxrossi8228_01_d2_2",
+      "name": "135_maxrossi8228_01_D2_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8310036980399147,
+          "pitch": 0.742440548814141,
+          "rotation": 0,
+          "target": "2-135_maxrossi8228_01_pasillo_2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
